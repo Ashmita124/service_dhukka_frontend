00b49ac01bb8e5e5fe05c1449cdf1f77e7686011
Ashmita124 <126133726+Ashmita124@users.noreply.github.com>
2025-03-02 16:53:28 +0545

    productview completed

--- src/components/ProductView/productView.jsx
+++ src/components/ProductView/productView.jsx
@@ -17,7 +17,7 @@ const ProductDetailComponent = ({ product }) => {
   const handleBuyNow = () => {
     alert("Redirecting to checkout...");
   };
-
+// 
   return (
     <div className="min-h-screen bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
       {showSuccessAlert && (
